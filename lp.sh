bf0a1b854bd4291f1a51e5ab098c6d20a5bfc3db
wr <86941613+wr0x00@users.noreply.github.com>
2025-07-07 09:09:47 +0800

    Create lp.sh

---
+++ lp.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install lxqt-core
+sudo apt install lxqt
+sudo apt install tigervnc-standalone-server tigervnc-common
+vncserver :1
+vncserver -geometry 1200x700
+git clone https://github.com/novnc/noVNC.git
+cd noVNC/utils
+./novnc_proxy --vnc localhost:5901
