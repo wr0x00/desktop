c8538e12f1013260e45d13317065fd7be032fb69
wr <86941613+wr0x00@users.noreply.github.com>
2025-07-07 01:26:39 +0000

    11

--- lp.sh
+++ lp.sh
@@ -7,3 +7,6 @@ vncserver -geometry 1200x700
 git clone https://github.com/novnc/noVNC.git
 cd noVNC/utils
 ./novnc_proxy --vnc localhost:5901
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
